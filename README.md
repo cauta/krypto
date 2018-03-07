5cbb7b1da1e07fa1072c6275d4cc0330d434241a
drbillnguyen <33407019+drbillnguyen@users.noreply.github.com>
2018-03-07 12:39:17 +0700

    Update README + open test source

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # krypto
+## init project
+```
+npm install -g truffle
+npm install
+```
+## start enviroment
+```
+truffle develop
+```
+## Run test
+local network:
+```
+truffle test --network dev
+```
+ropsten testnet:
+```
+truffle test --network ropsten
+```
+## Run migration
+```
+truffle migrate --network ropsten
+```
--- test/deployment.js
+++ test/deployment.js
@@ -2,24 +2,22 @@ var krypto = artifacts.require("./Krypto.sol");
 var web3 = require("Web3")
 
 contract('Krypto', function(accounts) {
-    // it("msg.sender should be the owner of this contract: ", function() {
-    //     return krypto.deployed().then(function(instance) {
-    //         return instance.owner.call();
-    //     }).then(function(owner) {
-    //         console.log("the owner of this contract is: " + owner);
-    //         assert.equal(owner, accounts[0], "first token is not own by owner");
-    //     });
-    //     // return isValid;
-    // });
+    it("msg.sender should be the owner of this contract: ", function() {
+        return krypto.deployed().then(function(instance) {
+            return instance.owner.call();
+        }).then(function(owner) {
+            console.log("the owner of this contract is: " + owner);
+            assert.equal(owner, accounts[0], "first token is not own by owner");
+        });
+        // return isValid;
+    });
 
-    // it("balance of owner should be 1,000,000,000 * 10^10: ", function() {
-    //     return krypto.deployed().then(function(instance) {
-    //         return instance.balanceOf.call(accounts[0]);
-    //     }).then(function(balance) {
-    //         console.log("balance of Owner is: " + balance);
-    //         assert.equal(balance, "10000000000000000000", "balance of owner is wrong");
-    //     });
-    // });
-
-
+    it("balance of owner should be 1,000,000,000 * 10^10: ", function() {
+        return krypto.deployed().then(function(instance) {
+            return instance.balanceOf.call(accounts[0]);
+        }).then(function(balance) {
+            console.log("balance of Owner is: " + balance);
+            assert.equal(balance, "10000000000000000000", "balance of owner is wrong");
+        });
+    });
 });
